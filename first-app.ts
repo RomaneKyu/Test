6154f50653a4edd03f2d06a0c0da16791c2c9179
RomaneKyu <romane.kulesza@kyu.fr>
2025-02-11 09:55:43 +0100

    next commit

--- first-app.ts
+++ first-app.ts
@@ -1,2 +1,36 @@
-let userName = "Max";
+let userName: string | number = "Max";
 console.log({ userName })
+
+let user: { name: string; age: number; isAdmin: boolean; };
+
+user = {
+    name: "Max",
+    age: 34,
+    isAdmin: true
+}
+
+let hobbies: string[]; //or Array<string>
+hobbies = ["sports", "cooking"];
+
+function add(a: number, b: number): number{
+    return a + b;
+}
+
+type AddFn = (a: number, b: number) => number
+
+function calculate(a:number, b:number, addFn: AddFn){
+    addFn(a, b);
+}
+
+calculate(2, 3, add);
+
+interface Credentials {
+    password: string;
+    email: string;
+}
+
+class AuthCreds implements Credentials {
+    email:string;
+    password: string;
+    username: string;
+}
---
+++ react-ts-basics/src/App.tsx
@@ -0,0 +1,10 @@
+import CourseGoal from './components/CourseGoal.tsx'
+import './App.css'
+
+export default function App() {
+  return (
+    <main>
+      <CourseGoal />
+    </main>
+  )
+}
